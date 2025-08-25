5f6295e5994f8bbfc8151689bb0ef1e856cd8aa5
Diogo Novo <47186591+Diogo-Novo@users.noreply.github.com>
2025-08-25 15:13:37 +0100

    Better launch configs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 using TCGProcessor.Data;
 using Microsoft.EntityFrameworkCore;
 using TCGProcessor.Middleware;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
 
@@ -43,6 +44,25 @@ builder.Services.AddDbContext<OSMGXProcessorDbContext>(options =>
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// API Documentation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MGX Processor API",
+        Version = "v1.0",
+        Description = "Enhanced MGX Processor API with Secure Endpoints",
+        Contact = new OpenApiContact
+        {
+            Name = "MGX Support Team",
+            Email = "shop@mobilegamesexchange.co.uk"
+        }
+    });
+
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -52,6 +72,13 @@ if (
     )
 {
     app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(c =>
+   {
+       c.SwaggerEndpoint("/swagger/v1/swagger.json", "MGX Processor API V1.0");
+       c.RoutePrefix = string.Empty;
+   });
 }
 
 app.UseHttpsRedirection();

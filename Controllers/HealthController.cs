b72ee287cc90c44b18ad9b112973968166c1f7f5
Diogo Novo <47186591+Diogo-Novo@users.noreply.github.com>
2025-08-25 14:39:46 +0100

    Updated health controller endpoint

--- Controllers/HealthController.cs
+++ Controllers/HealthController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace TCGProcessor.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class HealthController : ControllerBase
     {
         [HttpGet]
